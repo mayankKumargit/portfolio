1027e3013fc75ffbde2eb96fc42b5b43d3312d22
mayankKumar <kmayankkr@gmail.com>
2023-05-23 12:15:46 +0530

    committed updated3 portfolio.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,11 +11,11 @@ function Contact() {
                 <div className='flex justify-center items-center'>
                     <form action='https://getform.io/f/bce1ba7b-64dc-452c-870c-bd1f4e85c78e' 
                             className='flex flex-col w-full md:w-1/2' method='POST'>
-                        <input type='text' name='name' placeholder='enter your name'
+                        <input type='text' name='name' placeholder='enter your name' required
                                 className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></input>
-                        <input type='text' name='email' placeholder='enter your email'
+                        <input type='text' name='email' placeholder='enter your email' required
                                 className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></input>
-                        <textarea rows={10} name='message' placeholder='enter your message'
+                        <textarea rows={10} name='message' placeholder='enter your message' required
                                 className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
                         <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8
                                             mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
